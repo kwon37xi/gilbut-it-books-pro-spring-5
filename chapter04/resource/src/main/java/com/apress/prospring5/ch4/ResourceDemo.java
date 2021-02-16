139b4b477f0d21433e169bf7790c1a3b6c07ad0c
KwonNam Son <kwon37xi@gmail.com>
2021-02-17 00:03:05 +0900

    ResourceLoader, ResourceLoaderAware 를 사용하자.

--- chapter04/resource/src/main/java/com/apress/prospring5/ch4/ResourceDemo.java
+++ chapter04/resource/src/main/java/com/apress/prospring5/ch4/ResourceDemo.java
@@ -4,21 +4,27 @@ import java.io.File;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
 
 public class ResourceDemo {
     public static void main(String... args) throws Exception{
-        ApplicationContext ctx = new ClassPathXmlApplicationContext();
+        ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext();
+        ctx.refresh();
 
         File file = File.createTempFile("test", "txt");
         file.deleteOnExit();
 
-        Resource res1 = ctx.getResource("file://" + file.getPath());
+        // ResourceLoaderAware 로 resourceLoader 를 주입받거나 ResourceLoader 를 직접 사용한다.
+        // ctx.getBean(ResourceLoader.class); 는 실패하지만, 실제 애플리케이션에서는 주입 받으려고 하면 주입이 된다.
+        ResourceLoader resourceLoader = ctx;
+
+        Resource res1 = resourceLoader.getResource("file://" + file.getPath());
         displayInfo(res1);
 
-        Resource res2 = ctx.getResource("classpath:test.txt");
+        Resource res2 = resourceLoader.getResource("classpath:test.txt");
         displayInfo(res2);
 
-        Resource res3 = ctx.getResource("http://www.google.com");
+        Resource res3 = resourceLoader.getResource("http://www.google.com");
         displayInfo(res3);
     }
 
