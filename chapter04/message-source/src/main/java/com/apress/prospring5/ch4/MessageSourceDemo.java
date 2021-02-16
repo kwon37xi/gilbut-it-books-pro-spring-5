59b028bc9489fa9307230165647bcaedcbea5f21
KwonNam Son <kwon37xi@gmail.com>
2021-02-16 23:54:57 +0900

    MessageSource 로 처리

--- chapter04/message-source/src/main/java/com/apress/prospring5/ch4/MessageSourceDemo.java
+++ chapter04/message-source/src/main/java/com/apress/prospring5/ch4/MessageSourceDemo.java
@@ -2,6 +2,7 @@ package com.apress.prospring5.ch4;
 
 import java.util.Locale;
 
+import org.springframework.context.MessageSource;
 import org.springframework.context.support.GenericXmlApplicationContext;
 
 public class MessageSourceDemo {
@@ -12,15 +13,19 @@ public class MessageSourceDemo {
 
         // MessageSourceAware 로 주입받거나, MessageSource messageSource 를 직접 주입받는게 낫다.
 
+        MessageSource messageSource = ctx.getBean(MessageSource.class);
+
+        System.out.println("messageSource class : " + messageSource.getClass());
+
         Locale english = Locale.ENGLISH;
         Locale korean = new Locale("ko", "KR");
 
-        System.out.println(ctx.getMessage("msg", null, english));
-        System.out.println(ctx.getMessage("msg", null, korean));
+        System.out.println(messageSource.getMessage("msg", null, english));
+        System.out.println(messageSource.getMessage("msg", null, korean));
 
-        System.out.println(ctx.getMessage("nameMsg", new Object[] { "John",
+        System.out.println(messageSource.getMessage("nameMsg", new Object[] { "John",
                 "Mayer" }, english));
-        System.out.println(ctx.getMessage("nameMsg", new Object[] { "John",
+        System.out.println(messageSource.getMessage("nameMsg", new Object[] { "John",
                 "Mayer" }, korean));
 
         ctx.close();
